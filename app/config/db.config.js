21971d6e71ce71f0202422241462e2ad1a2820e3
Tejas Ozarkar <tejozarkar@gmail.com>
2022-07-19 21:08:38 +0530

    heroku database config added

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "root",
-    DB: "testdb",
+    HOST: "ec2-34-235-198-25.compute-1.amazonaws.com",
+    USER: "qgwyelceyghnkv",
+    PASSWORD: "360cf698578e02480eb65370b095dea676002515c6a9daf27b02e5fedd5f9ff0",
+    DB: "d72m2s767dhse3",
     dialect: "postgres",
     pool: {
         max: 5,
